0a44b9393d0b4c9a4a8cacdf38b819325fa86f8d
robson-carvalho <robsoncarvalho.tech@gmail.com>
2025-01-28 14:36:30 -0300

    feat: adiciona skeleton

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -4,6 +4,7 @@ import { BodyLayout } from "@/layouts/Body";
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router";
 import { api } from "@/services/api";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Content = () => {
   const navigate = useNavigate();
@@ -31,7 +32,17 @@ const Content = () => {
     <>
       <Header />
       <BodyLayout>
-        <MarkdownRenderer content={content} />
+        {content === "" ? (
+          <div className="flex flex-col gap-4">
+            <Skeleton className="h-12 w-max-[1440px]" />
+            <Skeleton className="h-24 w-max-[1440px]" />
+            <Skeleton className="h-24 w-max-[1440px]" />
+            <Skeleton className="h-24 w-max-[1440px]" />
+            <Skeleton className="h-24 w-max-[1440px]" />
+          </div>
+        ) : (
+          <MarkdownRenderer content={content} />
+        )}
       </BodyLayout>
     </>
   );
