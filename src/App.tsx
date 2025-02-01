d6d008f01410b47d83651c4d6b31787e60686516
robson-carvalho <robsoncarvalho.tech@gmail.com>
2025-02-01 00:27:00 -0300

    fix: bug deploy

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,8 @@ const App = () => {
               <Route path="/admin/settings" element={<AdminSettings />} />
             </Route>
 
+{/*
+ */}
             <Route element={<PrivateRoute />}>
               <Route path="/admin/class/:id" element={<Class />} />
             </Route>
