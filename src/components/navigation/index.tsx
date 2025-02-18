7ad0c123aa6e96dd9ab0fb47ef3bfb518cc60a4a
robson-carvalho <robsoncarvalho.tech@gmail.com>
2025-02-17 23:47:37 -0300

    fix: remove componente de views

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -23,29 +23,6 @@ const Navigation = ({ styles, onClose }: INavigationProps) => {
       <div className="w-full flex flex-col justify-center gap-8">
         <ul className="flex flex-col gap-y-0.5 sidebar-list-line:border-l border-dark/3 dark:border-light/2">
           <li className="flex flex-col">
-            <p className="text-[#58595C] group/tocA relative transition-colors flex flex-row justify-between p-1.5 pl-3 rounded-md straight-corners:rounded-none text-sm font-normal text-balance text-dark/8 hover:text-dark/9 hover:bg-dark/1 hover:before:bg-dark/3 dark:text-light/8 dark:hover:text-light/9 dark:hover:bg-light/1 dark:hover:before:bg-light/3 contrast-more:text-dark contrast-more:dark:text-light hover:contrast-more:text-dark dark:hover:contrast-more:text-light hover:contrast-more:ring-1 hover:contrast-more:ring-dark dark:contrast-more:hover:ring-light before:contents[] before:absolute before:inset-y-0 before:-left-px [&+div_a]:pl-5 sidebar-list-line:before:w-px sidebar-list-default:[&+div_a]:before:w-px sidebar-list-default:[&+div_a]:rounded-l-none sidebar-list-line:rounded-l-none">
-              Visualizações{" "}
-              <span className="flex flex-row gap-2">
-                <AnimatedNumbers
-                  key={Math.random()}
-                  includeComma
-                  transitions={(index) => ({
-                    type: "spring",
-                    duration: index + 0.5,
-                  })}
-                  animateToNumber={views}
-                  fontStyle={{
-                    color: "text-[#58595C]",
-                    fontSize: 12,
-                  }}
-                />
-
-                <Eye size={18} />
-              </span>
-            </p>
-          </li>
-
-          <li className="flex flex-col">
             <Anchor path="/" text="Treinamento para OBI" />
           </li>
 
